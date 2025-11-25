969c467f9e94cdb47e97a2400f1a2e74d57e1683
Unai Zorrilla Castro <unaizc@gmail.com>
2025-11-25 17:48:17 +0100

    Working again

--- apphost.cs
+++ apphost.cs
@@ -6,39 +6,39 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
 
+var redis = builder.AddAzureRedis("redis");
 
-if (builder.Environment.EnvironmentName == "Produdction")
-{
-    var redis = builder.AddAzureRedis("redis");
-}
-else
-{
-    builder.AddDockerComposeEnvironment("dc");
+// if (builder.Environment.EnvironmentName == "Produdction")
+// {
+// }    
+// else
+// {
+//     builder.AddDockerComposeEnvironment("dc");
 
-    var apiService = builder.AddProject("apiservice", "./ApiService")
-        .WithExternalHttpEndpoints();
+//     var apiService = builder.AddProject("apiservice", "./ApiService")
+//         .WithExternalHttpEndpoints();
 
-    var viteApp = builder.AddViteApp("frontend", "./frontend")
-        .WithExternalHttpEndpoints()
-        .WithReference(apiService);
+//     var viteApp = builder.AddViteApp("frontend", "./frontend")
+//         .WithExternalHttpEndpoints()
+//         .WithReference(apiService);
 
-    var redis = builder.AddAzureRedis("redis")
-        .RunAsContainer();
+//     var redis = builder.AddAzureRedis("redis")
+//         .RunAsContainer();
 
-#pragma warning disable ASPIREPIPELINES001
-#pragma warning disable ASPIREPIPELINES004
+// #pragma warning disable ASPIREPIPELINES001
+// #pragma warning disable ASPIREPIPELINES004
 
-    builder.Pipeline.AddStep("validate-deployment", async context =>
-    {
+//     builder.Pipeline.AddStep("validate-deployment", async context =>
+//     {
 
-        context.Logger.Log(Microsoft.Extensions.Logging.LogLevel.Information, 100, $"Environment Process: {Environment.CurrentDirectory}", null, (s, e) => s.ToString());
+//         context.Logger.Log(Microsoft.Extensions.Logging.LogLevel.Information, 100, $"Environment Process: {Environment.CurrentDirectory}", null, (s, e) => s.ToString());
 
 
-    }, dependsOn: "docker-compose-up-dc");
+//     }, dependsOn: "docker-compose-up-dc");
 
-#pragma warning restore ASPIREPIPELINES001
-#pragma warning restore ASPIREPIPELINES004
-}
+// #pragma warning restore ASPIREPIPELINES001
+// #pragma warning restore ASPIREPIPELINES004
+// }
 
 
 
