4d2cf7c095d0c70952c0bf4d27f1a958acd83cbd
Unai Zorrilla Castro <unaizc@gmail.com>
2025-11-25 17:38:51 +0100

    added tenantid

--- ApiService/obj/Release/net10.0/ApiService.AssemblyInfo.cs
+++ ApiService/obj/Release/net10.0/ApiService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+435f1031de0a240208069be2ddf76155494c479a")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
